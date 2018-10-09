a15bc27c3f81ce511bf0559e1adf360d238fc0db
Jasdeepsidhu2 <43177779+Jasdeepsidhu2@users.noreply.github.com>
2018-10-09 15:37:27 -0600

    day1

---
+++ day1 javascript/Age calculator/code.js
@@ -0,0 +1,5 @@
+var birthYear=1983;
+var futureYear= 2040;
+var age=futureYear-birthYear;
+
+console.log('You will be either ', age ,'or', age+1, 'in' ,futureYear);
---
+++ day1 javascript/Exercise(Fortune teller)/code.js
@@ -0,0 +1,9 @@
+// var noOfChildren= prompt('how many kids');
+var noOfChildren= 2;
+var parterName='John';
+var urLocation='Calgary';
+var jobTitle='Web Developer';
+
+
+
+console.log('You will be ' ,jobTitle, 'in ' , urLocation, 'and married to',parterName ,'with' ,noOfChildren,'kids');
