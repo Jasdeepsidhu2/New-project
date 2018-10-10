35561259e26722674b98cf3dc89b6e023f9ce8d4
imsandy9 <43177899+imsandy9@users.noreply.github.com>
2018-10-09 19:47:07 -0600

    Temperature converter

---
+++ day1 javascript/Temperature converter/code.js
@@ -0,0 +1,11 @@
+
+// celcius to fahrenheit converter
+var celciusTemp = 10;
+var farTemp = (celciusTemp * 9) / 5 + 32;
+console.log ("Temperature in fahrenheit ", farTemp);
+
+// fahrenheit to celcius converter
+
+var farTemp = 50;
+var celciusTemp = 5/9 * (farTemp - 32);
+console.log ("Temperature in Celcius ", celciusTemp );
